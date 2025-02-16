a227d6f98e6456568e6d98fa30789643195cee7c
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2025-02-16 12:56:08 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,54 +10,47 @@ GREEN = (0,255,0)
 score = 0
 lives = 3
 
-#creating the ship
-ship = Actor("ship")
-ship.pos = (WIDTH //2,HEIGHT-60)
+racquet = Actor("tennis raqcuet")
+racquet.pos = (WIDTH //2,HEIGHT-60)
 
-bullets = []
+balls = []
 enemies = []
 
-#creating the enemies
 for i in range(8):
-    enemy=Actor("bug")
+    enemy=Actor("fire")
     enemy.x = random.randint(0, WIDTH -100)
     enemy.y = random.randint(-100,0)
     enemies.append(enemy)
 
-#set enemy movement direction
 direction = 1
 
-#set enemy movement speed
 speed = 5
 
-#function to display score
 def display_score():
     global score, lives
     screen.draw.text(f"Score: {score}",(50,30))
     screen.draw.text(f"Lives: {lives}",(50,60))
 
-#function to handle key down events
+
 def on_key_down(key):
     if key == keys.SPACE:
-        #create a new bullet
-        bullet=Actor("bullet")
-        bullet.x = ship.x
-        bullet.y = ship.y - 50
-        bullets.append(bullet)
+        #create a new ball
+        ball=Actor("tennis ball")
+        ball.x = racquet.x
+        ball.y = racquet.y - 50
+        balls.append(ball)
 
-#function to update the game
+
 def update():
     global lives
     global score
     global direction
 
-    #move the ship left or right
     if keyboard.right:
-        ship.x += speed
-        if ship.x >= WIDTH:
-            ship.x = WIDTH
+        racquet.x += speed
+        if racquet.x >= WIDTH:
+            racquet.x = WIDTH
     elif keyboard.left:
-        ship.x -= speed
-        if ship.x <= 0:
-            ship.x = 0
-        
+        racquet.x -= speed
+        if racquet.x <= 0:
+            racquet.x = 0
